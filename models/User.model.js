29ce4b4fc0b60c9b0cd7ba0abc1d768abc82bebc
jsant12 <jsant12@wgu.edu>
2021-01-31 21:17:10 -0500

    Updating Branches

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const modelSchema = new Schema(
+const userSchema = new Schema(
   {
-    // model properties
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Please use a valid email address"]
+    },
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required']
+    }
   },
   {
     timestamps: true
   }
 );
 
-module.exports = model('Model', modelSchema);
+module.exports = model('User', userSchema);
