84bbce95bea87b848118d14006c015bedd2f20ca
Valerie Cevallos <valerie.cevallos@gmail.com>
2021-02-07 23:36:00 -0500

    Updated, enhanced uploader; added gallery updates

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -4,36 +4,53 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 //Model imports
+const Gallery = require('../models/Gallery.model')
 const User = require('../models/User.model');
 const Artwork = require('../models/Artwork.model');
 
+//required for file upload to Cloudinary
+const fileUploader = require("../configs/cloudinary.config");
+
 //*************** GALLERY ***************//
-const validImages = gallery.images.filter(image => image !== undefined)
+// const validImages = gallery.images.filter(image => image !== undefined)
 
 router.get("/gallery", (req, res, next) => {
   if(!req.session.currentUser) {
-    res.redirect('/gallery.hbs');
+    res.redirect('/auth/login');
   }
-  User.findById(req.session.currentUser._id)
-  .then((foundUserFromDB) => {
-    
-    console.log(foundUserFromDB);
-    res.render('gallery', {foundUserFromDB});
+  Gallery.find() 
+  .then((galleryFromDB) => {
+    res.render('gallery', { galleryFromDB })
   })
+  // console.log(galleryFromDB)
+  .catch((err) => console.log('Error, ', err))
   
-  .catch((err) => console.log("Error pulling User ID: ", err));
+  // console.log('===========>', req.session.currentUser._id);
+  // const { galleryTitle, galleryDescription, galleryTheme } = req.body;
+  // console.log(req.body);
+  // res.render('gallery', { galleryTitle, galleryDescription, galleryTheme })
+
 });
 
 
-router.post("/gallery:_id", (req, res, next) => {
+router.get('/gallery-new', (req, res, next) => {
   if(!req.session.currentUser) {
     res.redirect('/auth/login');
   }
-  User.findByIdAndUpdate(req.session.currentUser._id)
-  .then((foundUserFromDB) => {
-    
-    console.log(foundUserFromDB);
-    res.render('profile', {foundUserFromDB});
+    res.render('gallery-new', {userInSession: req.session.currentUser});
   })
-  
-  .catch((err) => cons
+
+router.post('/gallery-new', (req, res, next) => {
+  if(!req.session.currentUser) {
+    res.redirect('/auth/login');
+  }
+    const { galleryTitle, galleryDescription, galleryTheme } = req.body;
+    console.log(req.body);
+    Gallery.create({ galleryTitle, galleryDescription, galleryTheme, author: req.session.currentUser._id })
+    .then(() => {
+      res.redirect('/gallery');
+    })
+    .catch((err) => console.log('Error creating new Gallery: ', err))
+  });
+
+  module.exports = router;
