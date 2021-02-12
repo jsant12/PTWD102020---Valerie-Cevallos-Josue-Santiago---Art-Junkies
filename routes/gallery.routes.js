efdb13bb8e65ec9bd62e97152a9fe3023e66a941
jsant12 <jsant12@wgu.edu>
2021-02-11 21:35:13 -0500

    created gallery edit route and page

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -48,7 +48,7 @@ router.post('/gallery-new', (req, res, next) => {
     console.log(req.body);
     Gallery.create({ galleryTitle, galleryDescription, galleryTheme, image, author: req.session.currentUser._id })
     .then(() => {
-      res.redirect('/profile');
+      res.redirect('/gallery-artist');
     })
     .catch((err) => console.log('Error creating new Gallery: ', err))
   });
@@ -84,7 +84,7 @@ router.post('/gallery-new', (req, res, next) => {
     const { galleryTitle, galleryDescription, galleryTheme, image } = req.body
     Gallery.findByIdAndUpdate(req.params.galleryId, { galleryTitle, galleryDescription, galleryTheme, image }, {new: true})
     .then((updatedGallery) => {
-      res.redirect('/profile')
+      res.redirect('/gallery-artist')
     })
     .catch((err) => console.log('error Editing the Gallery', err))
   })
@@ -92,18 +92,18 @@ router.post('/gallery-new', (req, res, next) => {
   //***********DELETE GALLERY***********/
   router.post('/gallery-delete/:galleryId', (req, res, next) => {
     Gallery.findByIdAndRemove(req.params.galleryId)
-    .then(() => res.redirect('/profile'))
+    .then(() => res.redirect('/gallery-artist'))
     .catch((err) => console.log('Error deleting the gallery', err))
   });
 
   //*************** GALLERY DETAILS***************//
 // const validImages = gallery.images.filter(image => image !== undefined)
 
-router.get("/gallery-details/:galleryID", (req, res, next) => {
+router.get("/gallery-details/:galleryId", (req, res, next) => {
   if(!req.session.currentUser) {
-    res.redirect('/auth/login');
+    res.redirect('/gallery-details');
   }
-  Gallery.findById(req.params.galleryID)
+  Gallery.findById(req.params.galleryId)
   .populate("image")
   .populate("author") 
   .then((galleryFromDB) => {
@@ -113,4 +113,11 @@ router.get("/gallery-details/:galleryID", (req, res, next) => {
   .catch((err) => console.log('Error, ', err))
 });
 
+router.get("/gallery-artist/", (req, res, next) => {
+  Gallery.find({ author: req.session.currentUser} )
+  .then((galleryFromDB) => {
+    res.render('gallery-artist', { galleryFromDB })
+  })
+  .catch((err) => console.log('Error, ', err))
+});
   module.exports = router;
