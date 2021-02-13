fd90648ee68d00248b5f8967fdee6b2bda527271
jsant12 <jsant12@wgu.edu>
2021-02-12 23:46:11 -0500

    Corrected app name to Art Junkies

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Art Junkie';
+app.locals.title = 'Art Junkies';
 
 // const index = require('./routes/index');
 // app.use('/', index);
